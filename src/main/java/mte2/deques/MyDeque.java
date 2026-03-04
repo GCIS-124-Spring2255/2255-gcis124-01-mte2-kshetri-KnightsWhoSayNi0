2b118fd4eeac3db55779e1c10ce9c9e399594d93
John Godman <johngodman006@gmail.com>
2026-03-04 10:46:00 -0500

    linked list impl

--- src/main/java/mte2/deques/MyDeque.java
+++ src/main/java/mte2/deques/MyDeque.java
@@ -4,27 +4,54 @@
 
 package mte2.deques;
 
-// import java.util.LinkedList;
-// import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Iterator;
 
-public class MyDeque {
-// public class MyDeque<E> implements Deque<E> {
+public class MyDeque<E> implements Deque<E> {
     
-    // private final LinkedList<E> list;
-    // public MyDeque() {    list = new LinkedList<>();    }
+    /*
+     * You may use the data structure of your choice, including those provided
+     * to you in this repository and the JCF implementations of the data 
+     * structures introduced in our units 5 and 6
+     * 
+     * so I can just use the linked list we were given, right?
+     */
 
-    // ... for enqueueFront ...
+    private final LinkedList<E> list;
 
-    // ... for enqueueBack ...
+    public MyDeque() {
+        list = new LinkedList<>();
+    }
 
-    // ... for dequeueFront ...
+    @Override
+    public void enqueueFront(E element) {
+        list.addFirst(element);
+    }
 
-    // ... for dequeueBack ... 
+    @Override
+    public void enqueueBack(E element) {
+        list.addLast(element);
+    }
 
-    // ... for size ...
+    @Override
+    public E dequeueFront() {
+        return list.pollFirst();
+    }
 
-    // ... for iterator ...
+    @Override
+    public E dequeueBack() {
+        return list.pollLast();
+    }
 
+    @Override
+    public int size() {
+        return list.size();
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return list.iterator();
+    }
 
     public static void main(String[] args) { 
 
