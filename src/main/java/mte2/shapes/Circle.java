920ecff4a43183126deca89dd6fdaae7780fc2b2
John Godman <johngodman006@gmail.com>
2026-03-04 10:25:08 -0500

    circle

--- src/main/java/mte2/shapes/Circle.java
+++ src/main/java/mte2/shapes/Circle.java
@@ -4,14 +4,23 @@
 
 package mte2.shapes;
 
-public class Circle {
-// public class Circle implements Shape {
+public class Circle implements Shape {
     
-    // private final double radius;
-    // public Circle(double radius) {    this.radius = radius;    }
+    private final double radius;
 
-    // @Override
-    // @Override
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public double perimeter() {
+        return 2 * Math.PI * radius;
+    }
+
+    @Override
+    public double area() {
+        return Math.PI * Math.pow(radius, 2);
+    }
 
     public static void main(String[] args) {
         
